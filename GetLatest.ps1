5c61d381ae2d546d39238becc64170403d31f88c
Andrew Beasley <alloydbeasley@gmail.com>
2020-06-15 20:22:09 -0500

    adding script

---
+++ GetLatest.ps1
@@ -0,0 +1,22 @@
+﻿cd C:\code\BUXTON_ANALYTICS_PLATFORM
+
+$currentBranch = git branch --show-current
+$currentChanges = (git status -s |select-string -Pattern "^\s*[A|\?|D|M]" -AllMatches).Matches.Count
+
+$shouldUserCommit = Read-Host "This branch has unstaged changes. Would you like to stage and commit? [Y/N] "
+
+if($currentChanges > 0) {
+    $shouldUserCommit = Read-Host "This branch has unstaged changes. Would you like to stage and commit? [Y/N] "
+        
+    if($shouldUserCommit -eq "Y") {        
+        git add . 
+        Read-Host Write-Verbose("Committing changes from {0}" -f $currentBranch)
+        git commit -m "comitting latest"
+    }        
+}
+
+
+if($currentBranch -ne "master") {    
+    git checkout master
+    $currentBranch = git branch --show-current
+}
